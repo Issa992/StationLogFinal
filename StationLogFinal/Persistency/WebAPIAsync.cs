65f8b09f3084b05074ff991cbae910ae3a19f976
Patrik Ando <patr589d@edu.easj.dk>
2018-04-21 11:44:11 +0200

    ADDED : HTTP Request

---
+++ StationLogFinal/Persistency/WebAPIAsync.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace myGenericDBEFandWSMovies
+{
+    public class WebAPIAsync<T> : IWebAPIAsync<T> where T : class
+    {
+        #region Instance fields
+        private string _serverURL;
+        private string _apiPrefix;
+        private string _apiID;
+        private HttpClientHandler _httpClientHandler;
+        private HttpClient _httpClient;
+        #endregion
+
+        #region Constructor
+        public WebAPIAsync(string serverURL, string apiPrefix, string apiID)
+        {
+            _serverURL = serverURL;
+            _apiID = apiID;
+            _apiPrefix = apiPrefix;
+            _httpClientHandler = new HttpClientHandler();
+            _httpClientHandler.UseDefaultCredentials = true;
+            _httpClient = new HttpClient(_httpClientHandler);
+            _httpClient.BaseAddress = new Uri(_serverURL);
+        }
+        #endregion
+
+        #region Methods
+        public async Task Delete(int key)
+        {
+            HttpClient client = new HttpClient();
+            string url = _serverURL + "/" + _apiPrefix + "/" + _apiID + "/" + key;
+            CancellationToken cancellationToken = new CancellationToken();
+            HttpResponseMessage response = await client.DeleteAsync(url, cancellationToken);
+            response.EnsureSuccessStatusCode();
+        }
+
+        public async Task<List<T>> Load()
+        {
+            HttpClient client = new HttpClient();
+            string url = _serverURL + "/" + _apiPrefix + "/" + _apiID;
+            string response = await client.GetStringAsync(url);
+            var movies = JsonConvert.DeserializeObject<List<T>>(response);
+            return movies;
+        }
+
+        public async Task<T> Read(int key)
+        {
+            HttpClient client = new HttpClient();
+            string url = _serverURL + "/" + _apiPrefix + "/" + _apiID + "/" + key;
+            string response = await client.GetStringAsync(url);
+            var movie = JsonConvert.DeserializeObject<T>(response);
+            return movie;
+        }
+
+        public async Task Update(int key, T obj)
+        {
+            HttpClient client = new HttpClient();
+            string url = _serverURL + "/" + _apiPrefix + "/" + _apiID + "/" + key;
+            string postBody = JsonConvert.SerializeObject(obj);
+            HttpResponseMessage response = await client.PutAsync(url, new StringContent(postBody, Encoding.UTF8, "application/json"));
+            response.EnsureSuccessStatusCode();
+        }
+
+        public async Task Create(int key, T obj)
+        {
+            string url = _serverURL + "/" + _apiPrefix + "/" + _apiID + "/" + key;
+            string postBody = JsonConvert.SerializeObject(obj);
+            HttpClient client = new HttpClient();
+            client.DefaultRequestHeaders.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            HttpResponseMessage response = await client.PostAsync(url, new StringContent(postBody, Encoding.UTF8, "application/json"));
+            response.EnsureSuccessStatusCode();
+
+        }
+        #endregion
+    }
+}
