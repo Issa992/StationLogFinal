6f6b9d01e53be757878b23c6e18e0a2bd941897c
Abdullah Nofal Issa <abdu2997@edu.easj.dk>
2018-04-23 12:35:07 +0200

    ScaffoldClasses

---
+++ StationLogWebService/Controllers/MeasurementsController.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using StationLogWebService;
+
+namespace StationLogWebService.Controllers
+{
+    public class MeasurementsController : ApiController
+    {
+        private Model1DBContext db = new Model1DBContext();
+
+        // GET: api/Measurements
+        public IQueryable<Measurement> GetMeasurements()
+        {
+            return db.Measurements;
+        }
+
+        // GET: api/Measurements/5
+        [ResponseType(typeof(Measurement))]
+        public IHttpActionResult GetMeasurement(int id)
+        {
+            Measurement measurement = db.Measurements.Find(id);
+            if (measurement == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(measurement);
+        }
+
+        // PUT: api/Measurements/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutMeasurement(int id, Measurement measurement)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != measurement.MeasurementId)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(measurement).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!MeasurementExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Measurements
+        [ResponseType(typeof(Measurement))]
+        public IHttpActionResult PostMeasurement(Measurement measurement)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Measurements.Add(measurement);
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateException)
+            {
+                if (MeasurementExists(measurement.MeasurementId))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtRoute("DefaultApi", new { id = measurement.MeasurementId }, measurement);
+        }
+
+        // DELETE: api/Measurements/5
+        [ResponseType(typeof(Measurement))]
+        public IHttpActionResult DeleteMeasurement(int id)
+        {
+            Measurement measurement = db.Measurements.Find(id);
+            if (measurement == null)
+            {
+                return NotFound();
+            }
+
+            db.Measurements.Remove(measurement);
+            db.SaveChanges();
+
+            return Ok(measurement);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool MeasurementExists(int id)
+        {
+            return db.Measurements.Count(e => e.MeasurementId == id) > 0;
+        }
+    }
+}
