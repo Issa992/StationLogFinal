d6cb561810d646c38173b5f013d8f88a6479f0a4
yaamuu <yaamu122@gmail.com>
2018-04-29 22:20:18 +0200

    beggining

---
+++ StationLogFinal/Model/Measurment.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StationLogFinal.Model
+{
+    class Measurment
+    {
+        private int Id;
+        private string Description;
+        private double Value;
+        private int MonitorID;
+        private string Unit;
+        int UserID
+
+
+
+    }
+}
