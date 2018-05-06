ec49026ceadc699d6be9d9d57a37ff4ea285ab4b
yaamuu <yaamu122@gmail.com>
2018-05-06 13:50:59 +0200

    Added some functionality

--- StationLogFinal/Model/LogsHandler.cs
+++ StationLogFinal/Model/LogsHandler.cs
@@ -3,19 +3,61 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using StationLogFinal.ViewModel;
 
 namespace StationLogFinal.Model
 {
-    public class LogsHandler
+    class LogsHandler
     {
+        private Measurment measurment;
         private Comment comment;
-        public List<Comment> ShowDate;
-
-        public void ShowByDate(DateTime time)
+        public LogsViewModel LogsViewModel { get; set; }
+        public void AddMeasurment()
         {
-           var result = ShowDate.Find(x => x.date == time);
-            ShowDate = new List<Comment> {result};
+            measurment = new Measurment
+            {
+                ID = LogsViewModel.NewMeasurment.ID,
+                unit = LogsViewModel.NewMeasurment.unit,
+                Description = LogsViewModel.NewMeasurment.Description,
+                MonitorID = LogsViewModel.NewMeasurment.MonitorID,
+                UserID = LogsViewModel.NewMeasurment.UserID,
+                Value = LogsViewModel.NewMeasurment.Value
+            };
+            LogsViewModel.Measurments.Add(measurment);
         }
+
+        public void DeleteMeasurment()
+        {
+
+            LogsViewModel.Measurments.Remove(LogsViewModel.SelectedMeasurment);
+        }
+
+        public void AddComment()
+        {
+            comment = new Comment
+            {
+                //rewrite to add id. logid, userid automatically + db
+                ID = LogsViewModel.NewComment.ID,
+                LogID = LogsViewModel.NewComment.LogID,
+                comment = LogsViewModel.NewComment.comment,
+                time = DateTime.Now,
+                UserID = LogsViewModel.NewComment.UserID
+
+            };
+            LogsViewModel.Comments.Add(comment);
+
+        }
+
+        public void DeleteComment()
+        {
+            LogsViewModel.Comments.Remove(LogsViewModel.SelectedComment);
+        }
+
+        public LogsHandler(LogsViewModel logsViewModel)
+        {
+            LogsViewModel = logsViewModel;
+        }
+
 
     }
 }
--- StationLogFinal/Persistency/WebAPIAsync.cs
+++ StationLogFinal/Persistency/WebAPIAsync.cs
@@ -14,76 +14,76 @@ namespace myGenericDBEFandWSMovies
 {
     public class WebAPIAsync<T>
     {
-        //#region Instance fields
-        //private string _serverURL;
-        //private string _apiPrefix;
-        //private string _apiID;
-        //private HttpClientHandler _httpClientHandler;
-        //private HttpClient _httpClient;
-        //#endregion
+        #region Instance fields
+        private string _serverURL;
+        private string _apiPrefix;
+        private string _apiID;
+        private HttpClientHandler _httpClientHandler;
+        private HttpClient _httpClient;
+        #endregion
 
-        //#region Constructor
-        //public WebAPIAsync(string serverURL, string apiPrefix, string apiID)
-        //{
-        //    _serverURL = serverURL;
-        //    _apiID = apiID;
-        //    _apiPrefix = apiPrefix;
-        //    _httpClientHandler = new HttpClientHandler();
-        //    _httpClientHandler.UseDefaultCredentials = true;
-        //    _httpClient = new HttpClient(_httpClientHandler);
-        //    _httpClient.BaseAddress = new Uri(_serverURL);
-        //}
-        //#endregion
+        #region Constructor
+        public WebAPIAsync(string serverURL, string apiPrefix, string apiID)
+        {
+            _serverURL = serverURL;
+            _apiID = apiID;
+            _apiPrefix = apiPrefix;
+            _httpClientHandler = new HttpClientHandler();
+            _httpClientHandler.UseDefaultCredentials = true;
+            _httpClient = new HttpClient(_httpClientHandler);
+            _httpClient.BaseAddress = new Uri(_serverURL);
+        }
+        #endregion
 
-        //#region Methods
-        //public async Task Delete(int key)
-        //{
-        //    HttpClient client = new HttpClient();
-        //    string url = _serverURL + "/" + _apiPrefix + "/" + _apiID + "/" + key;
-        //    CancellationToken cancellationToken = new CancellationToken();
-        //    HttpResponseMessage response = await client.DeleteAsync(url, cancellationToken);
-        //    response.EnsureSuccessStatusCode();
-        //}
+        #region Methods
+        public async Task Delete(int key)
+        {
+            HttpClient client = new HttpClient();
+            string url = _serverURL + "/" + _apiPrefix + "/" + _apiID + "/" + key;
+            CancellationToken cancellationToken = new CancellationToken();
+            HttpResponseMessage response = await client.DeleteAsync(url, cancellationToken);
+            response.EnsureSuccessStatusCode();
+        }
 
-        //public async Task<ObservableCollection<StationTask>> Load()
+        //public async Task<ObservableCollection<<T>> Load()
         //{
         //    HttpClient client = new HttpClient();
         //    string url = _serverURL + "/" + _apiPrefix + "/" + _apiID;
-        //    string response = await client.GetStringAsync(url);
-           
-        //    ObservableCollection<StationTask> tasks = JsonConvert.DeserializeObject<ObservableCollection<StationTask>>(response);
+        //    string response = client.GetStringAsync(url);
+
+        //    ObservableCollection<T> tasks = JsonConvert.DeserializeObject<ObservableCollection<T>>(response);
         //    return tasks;
         //}
 
-        //public async Task<T> Read(int key)
-        //{
-        //    HttpClient client = new HttpClient();
-        //    string url = _serverURL + "/" + _apiPrefix + "/" + _apiID + "/" + key;
-        //    string response = await client.GetStringAsync(url);
-        //    var tasks = JsonConvert.DeserializeObject<T>(response);
-        //    return tasks;
-        //}
+        public async Task<T> Read(int key)
+        {
+            HttpClient client = new HttpClient();
+            string url = _serverURL + "/" + _apiPrefix + "/" + _apiID + "/" + key;
+            string response = await client.GetStringAsync(url);
+            var tasks = JsonConvert.DeserializeObject<T>(response);
+            return tasks;
+        }
 
-        //public async Task Update(int key, T obj)
-        //{
-        //    HttpClient client = new HttpClient();
-        //    string url = _serverURL + "/" + _apiPrefix + "/" + _apiID + "/" + key;
-        //    string postBody = JsonConvert.SerializeObject(obj);
-        //    HttpResponseMessage response = await client.PutAsync(url, new StringContent(postBody, Encoding.UTF8, "application/json"));
-        //    response.EnsureSuccessStatusCode();
-        //}
+        public async Task Update(int key, T obj)
+        {
+            HttpClient client = new HttpClient();
+            string url = _serverURL + "/" + _apiPrefix + "/" + _apiID + "/" + key;
+            string postBody = JsonConvert.SerializeObject(obj);
+            HttpResponseMessage response = await client.PutAsync(url, new StringContent(postBody, Encoding.UTF8, "application/json"));
+            response.EnsureSuccessStatusCode();
+        }
 
-        //public async Task Create(int key, T obj)
-        //{
-        //    string url = _serverURL + "/" + _apiPrefix + "/" + _apiID + "/" + key;
-        //    string postBody = JsonConvert.SerializeObject(obj);
-        //    HttpClient client = new HttpClient();
-        //    client.DefaultRequestHeaders.Clear();
-        //    client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-        //    HttpResponseMessage response = await client.PostAsync(url, new StringContent(postBody, Encoding.UTF8, "application/json"));
-        //    response.EnsureSuccessStatusCode();
+        public async Task Create(int key, T obj)
+        {
+            string url = _serverURL + "/" + _apiPrefix + "/" + _apiID + "/" + key;
+            string postBody = JsonConvert.SerializeObject(obj);
+            HttpClient client = new HttpClient();
+            client.DefaultRequestHeaders.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            HttpResponseMessage response = await client.PostAsync(url, new StringContent(postBody, Encoding.UTF8, "application/json"));
+            response.EnsureSuccessStatusCode();
 
-        //}
-        //#endregion
+        }
+        #endregion
     }
 }
