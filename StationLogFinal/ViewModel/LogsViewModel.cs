937d34209fc6ebd346f69b63100225c88db429e6
Abdullah Nofal Issa <abdu2997@edu.easj.dk>
2018-05-07 11:27:55 +0200

    Singleton

--- StationLogFinal/ViewModel/LogsViewModel.cs
+++ StationLogFinal/ViewModel/LogsViewModel.cs
@@ -7,7 +7,6 @@ using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Input;
-using myGenericDBEFandWSMovies;
 using StationLogFinal.Annotations;
 using StationLogFinal.Common;
 using StationLogFinal.Model;
