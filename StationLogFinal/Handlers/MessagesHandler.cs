856d17ef4ffc28bea4342d92ec85c05fae82827c
Adrianna Sliwak <adri1688@edu.easj.dk>
2018-06-18 00:41:26 +0200

    CodeBehind

---
+++ StationLogFinal/Handlers/MessagesHandler.cs
@@ -0,0 +1,47 @@
+﻿using StationLogFinal.Persistency;
+using StationLogWebApplication1;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StationLogFinal.Handlers
+{
+    public class MessagesHandler
+    {
+        private const string ServerUrl = "http://stationlogwebapplication120180521125426.azurewebsites.net";
+        private const string ApiPrefix = "api";
+        private const string ApiId = "Messages";
+        public WebAPIAsync<Message> WebApi = new WebAPIAsync<Message>(ServerUrl, ApiPrefix, ApiId);
+        public ObservableCollection<Message> Inbox;
+
+        public MessagesHandler()
+        {
+            LoadInbox();
+        }
+
+        public async void LoadInbox()
+        {
+           List<Message> templist = await WebApi.Load();
+            var inboxquerry = from mess in templist
+                              where mess.ReceiverId == SessionTools.CurrentSessioncs.GetCurrentUserID()
+                              select mess;
+            Inbox = new ObservableCollection<Message>(inboxquerry);
+        }
+        public async void SendMessage(Message mes)
+        {
+            await WebApi.Create(mes);
+        }
+        public void ReadMessage(Message mes)
+        {
+            mes.IsRead = true;
+        }
+        public async void DeleteMessage(Message mes)
+        {
+            await WebApi.Delete(mes.MessageId);
+            
+        }
+    }
+}
--- StationLogFinal/Handlers/NotificationHandler.cs
+++ StationLogFinal/Handlers/NotificationHandler.cs
@@ -1,93 +1,54 @@
-﻿using System;
+﻿using StationLogFinal.Persistency;
+using StationLogWebApplication1;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Collections.ObjectModel;
-using StationLogFinal.Persistency;
-using StationLogWebApplication1;
-using StationLogFinal.Handlers;
-using StationLogFinal.ViewModel;
-using Windows.UI.Popups;
 
 namespace StationLogFinal.Handlers
 {
-    class NotificationHandler
+    public class NotificationHandler
     {
-        #region property
-        const string ServerUrl = "http://stationlogsystemwebapplication20180521105958.azurewebsites.net";
-        const string ApiPrefix = "api";
-        const string NotApiId = "Notifications";
-        const string AlertsApiID = "Alerts";
-        WebAPIAsync<Notification> NotWebApi = new WebAPIAsync<Notification>(ServerUrl, ApiPrefix, NotApiId);
-        public static IWebAPIAsync<Notification> iWebApiAsyncNot = new WebAPIAsync<Notification>(ServerUrl, ApiPrefix, NotApiId);
-        WebAPIAsync<Alert> AlertWebApi = new WebAPIAsync<Alert>(ServerUrl, ApiPrefix, AlertsApiID);
-        public static IWebAPIAsync<Alert> iWebApiAsyncAler = new WebAPIAsync<Alert>(ServerUrl, ApiPrefix, AlertsApiID);
+        private const string ServerUrl = "http://stationlogwebapplication120180521125426.azurewebsites.net";
+        private const string ApiPrefix = "api";
+        private const string NotApiId = "Notifications";
+        private const string AllApiId = "Alerts";
+        public WebAPIAsync<Alert> AWebApi = new WebAPIAsync<Alert>(ServerUrl, ApiPrefix, AllApiId);
+        public WebAPIAsync<Notification> NWebApi = new WebAPIAsync<Notification>(ServerUrl, ApiPrefix, NotApiId);
+        public ObservableCollection<Notification> notifications;
+        public ObservableCollection<Alert> alerts;
+        
 
-        public NotificationsViewModel ViewModel;
-        #endregion
-        #region Constructor
-        public NotificationHandler (NotificationsViewModel VM)
+        public NotificationHandler()
         {
-            this.ViewModel = VM;
-        }
-        #endregion
-        #region Methods
-        public async void AddNotification()
-        {
-            try
-            {
-                Notification objectToPut = ViewModel.newNotification;
-                ViewModel.notificationCO.Add(objectToPut);
-                await NotWebApi.Create(objectToPut);
-            }
-            catch(Exception e)
-            {
-                MessageDialog popup = new MessageDialog($"Error occured while" +
-                    $" trying to add Notification./n{e.ToString()}");
-            }
-            
-        }
-        public async void AddAllert()
-        {
-            try
-            {
-                Alert objectToPut = ViewModel.newAlert;
-                ViewModel.alertsCO.Add(objectToPut);
-                await AlertWebApi.Create(objectToPut);
-            }
-            catch (Exception e)
-            {
-                MessageDialog popup = new MessageDialog($"Error occured while" +
-                    $" trying to add Alert./n{e.ToString()}");
-            }
-            
-        }
-        public string ReadNotification()
-        {
-            ViewModel.selectedNotification.IsRed = true;
-            return ViewModel.selectedNotification.Message;
-        }
-        public string ReadAlert()
-        {
-            ViewModel.selectedAlert.IsRed = true;
-            return ViewModel.selectedAlert.Message;
-        }
-        public int GetNumberOfNewNotification()
-        {
-            var result = from Notif in ViewModel.notificationCO
-                         where Notif.IsRed == false
-                         select Notif;
-            return result.Count<Notification>();
-        }
-        public int GetNumberOfNewAlerts()
-        {
-            var result = from alert in ViewModel.alertsCO
-                         where alert.IsRed = false || alert.IsToggled == true
-                         select alert;
-            return result.Count();
+            LoadAll();
         }
 
-        #endregion
+
+        public async Task<bool> LoadAll()
+        {
+            List<Notification> nots = await NWebApi.Load();
+            List<Alert> als = await AWebApi.Load();
+            return true;
+        }
+
+        public async void AddNotification(Notification not)
+        {
+            await NWebApi.Create(not);
+        }
+        public async void AddAllert(Alert alert)
+        {
+            await AWebApi.Create(alert);
+        }
+        public void ReadAlert (Alert alert)
+        {
+            alert.IsRed = true;
+        }
+        public void ReadNotification(Notification not)
+        {
+            not.IsRed = true;
+        }
     }
 }
